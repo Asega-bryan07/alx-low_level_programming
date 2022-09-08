19765b0a0dd3c43f70e057fc5a7d5dc8370c4419
Asega-bryan07 <Almasibryan07@gmail.com>
2022-09-09 02:23:35 +0300

    6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,26 @@
+# include <stdio.h>
+
+/**
+ * main - Entry block
+ *
+ * Return: Always goood
+ *
+ */
+int main(void)
+{
+	
+        int a = sizeof(char);
+	int b = sizeof(int);
+	int c = sizeof(int);
+	int d = sizeof(int);
+	int e = sizeof(float);
+
+	printf("Size of a char: %d byte(s)\n", a);
+	printf("Size of an int: %d byte(s)\n", b);
+	printf("Size of an int: %d byte(s)\n", c);
+	printf("Size of a long long int: %d byte(s)\n", d);
+	printf("Size of a float: %d byte(s)\n", e);
+	return (0);
+}
+
+
