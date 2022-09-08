953852049a4e08d529945c48db2b116b4e6d8bcf
Asega-bryan07 <Almasibryan07@gmail.com>
2022-09-09 00:44:41 +0300

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,7 @@
 #!/bin/bash
 #include <stdio.h>
-str main(void) {
-	printf("with proper grammar, but the outcome is a piece of art,")
+int main(void)
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
 }
