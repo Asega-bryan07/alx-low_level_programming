e53455f6d04fcfaddc791bee8ceba134b532c7ad
Asega-bryan07 <Almasibryan7@gmail.com>
2022-09-12 15:15:17 +0300

    8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,14 +10,14 @@ int main(void)
 {
 	char c;
 
-	for (c = '0'; c<= '9'; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
 		putchar(c);
 	}
-	for (c = 'a'; c <= 'e'; c++)
+	for (c = 'a'; c <= 'f'; c++)
 	{
 		putchar(c);
 	}
 	putchar(c);
-	return(0);
+	return (0);
 }
