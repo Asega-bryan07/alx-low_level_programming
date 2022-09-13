9e113baedb2402b780a64aba1fe71a3c5170a68d
Asega-bryan07 <Almasibryan7@gmail.com>
2022-09-14 00:48:37 +0300

    0,1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h'
+/**
+* main - alphabet function to print a to z
+*
+* Description: prints the alphabet, in lowercase
+* REturn: 0
+*/
+void print_alphabet(void);
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
