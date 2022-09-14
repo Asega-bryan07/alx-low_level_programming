f49f1c32b901780b348e85e331af103fb3f80382
Asega-bryan07 <Almasibryan7@gmail.com>
2022-09-14 16:32:03 +0300

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - function returns the absolute values for integers
+ *
+ * @n: takes integer type input for a function
+ * REturn: 0 success
+ */
+int _abs(int n)
+{
+	if (n < 0)
+
+		n = (-1) * n;
+	return (n);
+}
