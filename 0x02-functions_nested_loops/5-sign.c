775ba5150ab03fd384ede5f882a81a6968c9f652
Asega-bryan07 <Almasibryan7@gmail.com>
2022-09-14 16:19:57 +0300

    5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - print +1, 0, or -1 for n
+ *
+ * @n: takes integer type input for a function
+ * Return: +1 if +ve, 0 if 0, and -1 if negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43 + 0);
+		return (1);
+	}
+	else if
+	{
+		_putchar(48 + 0);
+		return (0);
+	}
+	else
+	{
+		_putchar(45 + 0);
+		return (-1);
+	}
+}
