3806cdc2a521ac41b00d41ff68bbbd1182568bee
Asega-bryan07 <Almasibryan7@gmail.com>
2022-09-14 22:56:31 +0300

    103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints the sum of fibonacci
+ *
+ * Description: Fibonacci
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long a = 0, b = 1, c = 0, sum = 0;
+
+	while (c <= 4000000)
+	{
+		c = a+b;
+		a = b;
+		b = c;
+		if ((a % 2) == 0)
+			sum += a;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
