a102ae7456f417c74b3d275f48e76002e58fea06
Asega-bryan07 <Almasibryan7@gmail.com>
2022-09-14 22:42:11 +0300

    102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * fibonacci - returns suit numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+
+	unsigned long x1 = 0, x2 = 1, x3;
+	for (c = 0; c < 50; c++)
+	{
+		x3 = x1 + x2;
+		printf("%lu", x3);
+		x1 = x2;
+		x2 = x3;
+		if (c == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+	return (0);
+}
