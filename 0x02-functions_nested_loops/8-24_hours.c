6950d830898cc8a2401138e72b9ccc8890555ecb
Asega-bryan07 <Almasibryan7@gmail.com>
2022-09-14 17:32:19 +0300

    8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
  * jack bauer - prints every minute of the day
+ * Descroption: function prints from 00:00 to 23:59 hrs
+ * Return: the time from 00:00 to 23:59
  */
 void jack_bauer(void)
 {
@@ -11,10 +13,10 @@ void jack_bauer(void)
 		for (min = 0; min <= 59; ++min)
 		{
 			_putchar((hr / 10) + 48);
-			_putchar((hr / 10) + 48);
+			_putchar((hr % 10) + 48);
 			_putchar(',');
 			_putchar((min / 10) + 48);
-			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
 			_putchar('\n');
 		}
 	}
