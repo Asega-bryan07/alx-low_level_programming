11b8661395fa22d60c65636ad91c652b7721ba1b
Asega-bryan07 <Almasibryan7@gmail.com>
2022-09-15 17:22:58 +0300

    8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_square - prints a square, followed by a new line
+ *
+ * @size: parameter
+ */
+void print_square(int size)
+{
+	int a, b;
+
+	if (size > 0)
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+			{
+				putchar('#');
+			}
+			putchar('#');
+			putchar('\n');
+		}
+	}
+	else
+	{
+		putchar('\n');
+	}
+}
