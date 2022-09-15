b4c511025633f019c37969ca67b950441b207cc8
Asega-bryan07 <Almasibryan7@gmail.com>
2022-09-15 11:36:53 +0300

    0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - testing a function that prints +ve, or -ve integers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
