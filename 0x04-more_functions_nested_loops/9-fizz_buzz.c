41af52d73233a0dc2a9d9661ab8d20909b263112
Asega-bryan07 <Almasibryan7@gmail.com>
2022-09-15 22:47:36 +0300

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,35 +6,22 @@
  */
 int main(void)
 {
-	int num = 1;
+	int num;
 
-	while (num++ < 100)
+	for (num = 1; num <= 100; ++num)
 	{
-		if ((num % 3 == 0) && (num % 5 == 0))
-		{
-			printf("FizzBuzz ");
-		}
-		else if ((num % 3) == 0)
-		{
-			printf("Fizz ");
-		}
-		else if ((num % 5) == 0)
-		{
-			printf("Buzz")
-		}
-		if (num != 100)
-		{
-			printf("Buzz ");
-		}
-		else
-		{
+		if (num % 3 == 0 && !(num % 5 == 0))
+			printf("Fizz");
+		else if (num % 5 == 0 && !(num % 3 == 0))
 			printf("Buzz");
-		}
+		else if (num % 3 == 0 && num % 5 == 0)
+			printf("FizzBuzz");
 		else
-		{
-			printf("%d ", num);
+			printf("%d", num);
+		if (num != 100)
+			printf(" ");
+		else
+			printf("\n");
 		}
-		printf("\n");
 		return (0);
-	}
 }
