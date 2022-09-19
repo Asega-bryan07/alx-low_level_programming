4433b646e168799cf9243ea388fe218f9ff9f5a6
Asega-bryan07 <Almasibryan7@gmail.com>
2022-09-20 00:17:15 +0300

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,12 +8,21 @@
  */
 void rev_string(char *s)
 {
-	int i, tmp, len = rev_string(s);
+	int i = 0, j = 0;
+	char str[500];
 
-	for (i = 0, i < len / 2; i++)
+		while (*(s + i))
 	{
-		tmp = *(s + i);
-		*(s + i) = *(s + len - i - 1);
-		*(s + len - i - 1) = tmp;
+		*(str + i) = *(s + i);
+		i++;
+	}
+	i = i - 1;
+	while (i >= 0)
+	{
+		*(s + i) = *(str + j);
+		j++;
+		i--;
 	}
 }
+
+
