ec33634f40ea721fb0a48fb38c3c4572cb072c7f
Asega-bryan07 <Almasibryan7@gmail.com>
2022-09-22 16:06:36 +0300

    6-cap_string.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -16,6 +16,8 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
-void print_line(char *c, int s, int l)
+void print_line(char *c, int s, int l);
+char *cap_string(char *str);
+int _strlen(char *s);
 
 #endif /*MAIN_H*/
