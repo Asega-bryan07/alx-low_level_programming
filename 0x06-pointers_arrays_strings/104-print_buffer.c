ee21c2cd828a9144a33894a56d3535ba53a2be25
Asega-bryan07 <Almasibryan7@gmail.com>
2022-09-22 18:20:46 +0300

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,8 +1,8 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_buffer - C function that prints the content of
- * an input number of bytes from a buffer
+ * print_line - prints bytes of a buffer from an input number
  * prints 10 bytes per line
  * starts with the position of first byte of line in hexadecimal (8 chars)
  * starting with 0
@@ -10,43 +10,54 @@
  * Each line ends with a new line \n
  * If size is 0 or less, the output should be a new line only \n
  *
+ * @c: buffer to print
+ * @s: bytes of buffer to print
+ * @l: line of buffer to prin
  * Return: void
- * print_buffer - prints a buffer
- * @b: buffer to be printed
+ */
+void print_line(char *c, int s, int l)
+{
+	int j, k;
+
+	for (j = 0; j <= 9; j++)
+	{
+		if (j <= s)
+			printf("%02x", c[l * 10 + j]);
+		else
+			printf("  ");
+		if (j % 2)
+			putchar(' ');
+	}
+	for (k = 0; k <= s; k++)
+	{
+		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
+			putchar(c[l * 10 + k]);
+		else
+			putchar('.');
+	}
+}
+/**
+ * print_buffer - prints a buffer with
+ * @b: parameter
  * @size: size of the buffer
  */
 void print_buffer(char *b, int size)
 {
-	int i = 0, j;
+	int i;
 
-	if (size < 0)
+	for (i = 0; i <= (size - 1) / 10 && size; i++)
 	{
-		printf('\n');
-		return;
-	}
-	while (i < size)
-	{
-		if (i % 10 == 0)
-			printf("%08x: ", i);
-		for (j = i; j < i + 9; j += 2)
+		printf("%08x: ", i * 10);
+		if (i < size / 10)
 		{
-			if ((j < size) && ((j + 1) < size))
-				printf("%02x%02x: ", b[j], b[j + 1]);
-			else
-			{
-				while (++j <= i + 10)
-					printf(" ");
-				printf(" ");
-			}
+			print_line(b, 9, i);
 		}
-		for (j = i; j < i + 9 && j < size; j++)
+		else
 		{
-			if (b[j] >= 32 && b[j] <= 126)
-				printf("%c", b[j]);
-			else
-				printf(".");
+			print_line(b, size % 10 - 1, i);
 		}
-		printf('\n');
-		i += 10;
+		putchar('\n');
 	}
+	if (size == 0)
+		putchar('\n');
 }
