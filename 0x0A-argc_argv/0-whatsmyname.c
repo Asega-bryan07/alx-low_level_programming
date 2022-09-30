012a1550b20882ea8f7fce2fe572cd70685cdae9
Asega-bryan07 <Almasibryan7@gmail.com>
2022-09-30 13:50:23 +0300

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints its name, followed by a new line
+ *
+ * @argc: argc parameter
+ * @argv: array of a command listed
+ * Return: 0
+ */
+int main(int argc, char argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the number of arguments passed into it
+ *
+ *
