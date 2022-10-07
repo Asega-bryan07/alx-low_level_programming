939ee0828e46d226eba7a4c7357dc8bd959effe8
Asega-bryan07 <Almasibryan7@gmail.com>
2022-10-07 11:08:00 -0700

    More malloc, free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdlib.h>
 
 /**
@@ -25,7 +24,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		if (new_size == 0)
 		{
 			free(ptr);
-			return (NULLL);
+			return (NULL);
 		}
 		else if (new_size == old_size)
 			return (ptr);
