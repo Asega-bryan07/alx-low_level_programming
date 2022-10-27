2df000c2ce729b58fcdff2563bc79d7cb85ac06e
Asega-bryan07 <Almasibryan7@gmail.com>
2022-10-27 05:42:04 -0700

    More Singly Linked Lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - function that inserts a new node at a given position
+ * insert_nodeint_at_index - function that
+ * inserts a new node at a given position
  *
  * @head: pointer to the head of the list
  * @idx: index of the list where the new node will be added
@@ -18,7 +19,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx != 0)
 	{
-		for (i = 0; i < idx -1 && h != NULL; i++)
+		for (i = 0; i < idx - 1 && h != NULL; i++)
 		{
 			h = h->next;
 		}
