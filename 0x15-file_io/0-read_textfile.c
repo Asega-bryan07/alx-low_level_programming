0971337ae54df1e8eb5bafcb109d59b4ab58df06
Asega-bryan07 <Almasibryan7@gmail.com>
2022-11-02 12:52:05 -0700

    File I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,6 @@
 #include "main.h"
-#include <stddef.h>
+#include <stdlib.h>
+#include <sys/stat.h>
 
 /**
  * read_textfile - a function that reads a text file and
@@ -11,26 +12,31 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters);
 {
-	int file, n_read, wrote;
+	int fd;
+	ssize_t lenr, lenw;
 	char *buffer;
 
-	buffer = malloc(sizeof(buffer) * (letters + letters));
-	if (filename == NULL || buffer == NULL)
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		clode(fd);
+		return (0);
+	}
+	lenr = read(fd, buffer, letters);
+	close(fd);
+	if (lenr == -1)
 	{
 		free(buffer);
 		return (0);
 	}
-	file = open(filename, O_RDONLY);
-	if (file == -1)
-		return (0);
-	n_read = read(file, buffer, letters);
-	if (n_read == -1)
-		return (0);
-	buffer[n_read] = '\0';
-	wrote = write(STDOUT_FILENO, buffer, n_read);
-	if (wrote != n_read)
-		return (0);
+	lenw = write(STDOUT_FILENO, buffer, lenr);
 	free(buffer);
-	close(file);
-	return (n_read);
+	if (lenr != lenw)
+		return (0);
+	return (lenw);
 }
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,20 +1,7 @@
 #include "main.h"
 #include <stddef.h>
+#include <string.h>
 
-/**
- * _strlen - counts the length of a string
- *
- * @str: string to be used
- * Return: length of the string
- **/
-int _strlen(char *str)
-{
-	int len = 0;
-
-	while (str[len] != '\0')
-		len++;
-	return (len);
-}
 /**
  * append_text_to_file - function that appends text at the end of a file.
  *
@@ -24,27 +11,24 @@ int _strlen(char *str)
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file, wrote;
+	int file;
+	int nletters;
+	int rwr;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 	file = open(filename, O_WRONLY | O_APPEND);
 	if (file == -1)
 		return (-1);
-	if (text_content != NULL)
+	if (text_content)
 	{
-		wrote = write(file, text_content, _strlen(text_content));
-		if (wrote == -1)
-		{
-			close(file);
+		for (nletters = 0; text_content[nletters]; nletters++)
+			;
+		rwr = write(file, text_content, nletters);
+
+		if (rwr == -1)
 			return (-1);
-		}
-		close(file);
-		return (-1);
 	}
-	else
-	{
-		close(file);
-		return (-1);
-	}
+	close(file);
+	return (-1);
 }
